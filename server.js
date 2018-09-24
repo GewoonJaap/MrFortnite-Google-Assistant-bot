ce675ee05fecbebd15a009864ea6bf1131462374
Glitch (mrfortnite) <none>
2018-09-24 16:58:41 +0000

    👮🍄 Updated with Glitch

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var admin = require('firebase-admin');
 // The Fortnite API key is written in a protected file that you can't access, request a key at: https://fortnitetracker.com/site-api
 var FortniteAPIKey = process.env.FORTNITEAPI
 // Google Drive feedback URL, used to save the feedback into google forms
-var GoogleDriveFeedbackURL = process.env.GOOGLEDRIVEURL
+var GoogleDriveFeedbackURL = process.env.GOOGLEDRIVEURL;
+var CommandLoggerURL = process.env.COMMANDLOGGERURL;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -41,6 +42,7 @@ app.post('/webhook', (req, res) => {
   
   
   if(req.body.queryResult.intent.displayName = "stats"){
+    commandlogging();
   
   let name = req.body.queryResult.parameters.any
   
@@ -362,7 +364,7 @@ app.post('/webhook', (req, res) => {
   
   if(intent == "Cube"){
 
-
+commandlogging();
     
     
     
@@ -463,6 +465,24 @@ app.post('/webhook', (req, res) => {
     
   }
   
+  
+  function commandlogging() {
+       let Text = req.body.queryResult.queryText
+
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+    var thxtext;
+    
+request.post(CommandLoggerURL, 
+    {form:{ 'entry.1325239568': intent, 'entry.761812201': Text }}, 
+    function(error, response, body){
+  console.log("-> Command logged!")
+
+  return;
+});
+}
+  
   if(intent == 'Feedback'){
   let error = req.body.queryResult.parameters.error
 let intenttext = req.body.queryResult.parameters.intenttext
@@ -506,7 +526,7 @@ request.post(GoogleDriveFeedbackURL,
   
   
   if(intent == 'Page 2' || intent == 'Force shop page 2'){
-    
+    commandlogging();
                   var options = {
       method: "GET",
       url: 'https://api.fortnitetracker.com/v1/store',
@@ -1615,6 +1635,7 @@ request.post(GoogleDriveFeedbackURL,
   }
   
   if(intent == 'shop'){
+    commandlogging();
 
       
               var options = {
@@ -2314,6 +2335,7 @@ request.post(GoogleDriveFeedbackURL,
   
   
   if(intent == "news"){
+    commandlogging();
     
           //EN playstation stats
       
